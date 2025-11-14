f1733f8a4044b10551fec2ce859e24bca43c4c66
Rehman Ali <rehmanali.pk60@gmail.com>
2025-11-14 15:38:05 +0500

    Add files via upload

---
+++ log.dart
@@ -0,0 +1,20 @@
+void main(){
+  int x;
+  x = 10;
+  int y;
+  y = 20;
+
+  // swap values of two variable
+
+  print ('beforeswaping');
+  print ("value of x: $x");
+ print ("value of y: $y");
+   x = x + y;  // 10 + 20 = 30
+   y = x - y;  // 30 - 20 = 10
+   x = x - y;  // 30 - 10 = 20
+ print ("print after swaping");
+  print ("value of x: $x");
+ print ("value of y: $y");
+
+
+}
